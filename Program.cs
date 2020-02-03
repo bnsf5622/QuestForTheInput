5abf8f2191a41a47cd64e0f2a818c27cb23d1539
chuck francis <chuck_francis10@yahoo.com>
2020-02-03 10:50:05 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace QuestForTheInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String yourName;
+            String yourQuest;
+            double VelocityOfSwallow;
+
+            // We can us Console.ReadLine as is with our strings because its default is string
+            Console.WriteLine("What is your name?");
+            yourName = Console.ReadLine();
+            
+            Console.WriteLine("What is your quest? ");
+            yourQuest = Console.ReadLine();
+
+            /* When we get to our double data type, we have to do a bit of type changing
+                 so we'll use Convert.ToDouble method and put ConsoleReadLine() as the argument. */
+            Console.WriteLine("What is the airspeed velocity of an unladed swallow?!?!?!");
+            VelocityOfSwallow = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("How do you know " + VelocityOfSwallow + " is correct, " + yourNameName + ", ");
+            Console.WriteLine
+        }
+    }
+}
